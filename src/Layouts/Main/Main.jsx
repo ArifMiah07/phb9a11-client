52a2885cc80f9673963a57ba813556913e372bd6
ArifMiah07 <arifmiah.me322@gmail.com>
2024-05-12 01:58:24 +0600

    primary routes setup done for Mian and home, login, register and also for error page

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 
 
 const Main = () => {
     return (
         <div>
             <h1>this is main</h1>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,33 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layouts/Main/Main";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home/Home";
+import LogIn from "../Pages/LogIn/LogIn";
+import Register from "../Pages/Register/Register";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/login',
+            element: <LogIn></LogIn>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        }
+      ]
+    },
+  ]);
 
 
-const Routes = () => {
-    return (
-        <div>
-            <h1>routes</h1>
-        </div>
-    );
-};
-
-export default Routes;
+  export default router;
